c662566b9d1ba7b605235cf145ceba2f3cb70f34
임승현 <limseung10@gmail.com>
2020-07-19 20:16:55 +0900

    multiSearch v.10

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+
+        <v-row justify="center" align="center" style="height:200px;">
+          <div class="back-grey">광고?</div>
+        </v-row>
+
+        <v-row>
+          <v-col cols=2 class="back-grey">
+            다각형 + 유저성향 + 배지?
+
+          </v-col>
+          <v-col>
+            <MultiSearch/>
+
+          </v-col>
+          <v-col cols=2 class="back-grey">
+            여기에 무엇 배치?
+
+          </v-col>
+        </v-row>
+
+        <v-row justify="center" align="center" style="height:200px;">
+          <div class="back-grey">광고?</div>
+        </v-row>
+
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import MultiSearch from './components/MultiSearch';
+
+export default {
+  name: 'App',
+
+  components: {
+    MultiSearch,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+
+.back-grey {
+  background-color: grey;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>

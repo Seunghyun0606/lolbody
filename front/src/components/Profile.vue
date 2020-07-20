7eef06a5bc41d26ee293c362d5faa3b0f08de7ff
KHR <gudfo19950@naver.com>
2020-07-20 14:21:05 +0900

    test

---
+++ front/src/components/Profile.vue
@@ -0,0 +1,97 @@
+<template>
+<v-app id="sandbox">
+    <v-main>
+        <v-container fluid>
+            <v-row algin="center" justify="center">
+                <v-col cols="10">
+                    <table width="100%">
+                        <tr>
+                            <td width="30%">
+                                <v-card height="150px" algin="center">
+                                    <div>
+                                        <v-avatar class="ma-3" size="70">
+                                            <v-img :src= "item.iconSrc"/>
+                                        </v-avatar>
+                                    </div>
+                                    <v-card-title class="headline" v-text="item.nickname"/>
+                                </v-card>
+                            </td>
+                            <td rowspan="2">
+                                <v-card>
+                                    <v-row>
+                                        <v-col cols="1">
+                                            <v-btn class="graph_btn">
+                                                LP
+                                            </v-btn>
+                                        </v-col>
+                                        <v-col cols="1">
+                                            <v-btn class="graph_btn"> 
+                                                총점
+                                            </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-card>
+                                
+                                <v-card style="margin-top:10px">
+                                    <RaderChart/>
+                                </v-card>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <v-card height="150px" algin="center">
+                                    <div>
+                                        <v-avatar class="ma-3" size="70">
+                                            <v-img :src="item.lankSrc" />
+                                        </v-avatar>
+                                    </div>
+                                </v-card>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+                    </table>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
+</v-app>
+</template>
+
+<script>
+import RaderChart from '@/components/RaderChart';
+
+export default {
+    name:'Profile',
+    components:{
+        RaderChart
+    },
+    data: ()=>({
+        item:{
+            nickname : 'BBluee',
+            iconSrc : require('@/assets/images/937.png'), 
+            lankSrc: require('@/assets/images/Emblem_Challenger.png'),
+
+        }
+    })
+}
+</script>
+
+<style>
+* {
+    padding : 0px;
+    margin : 0px;
+}
+div {
+    display : inline;
+}
+h2 {
+    display: inline;
+}
+.graph_btn {
+    margin-left: 10px;
+}
+td{
+    vertical-align: top;
+}
+</style> 
---
+++ front/src/components/RadarChart.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <VueApexCharts style="background-color: red;" type="radar" :options="chartOptions" :series="series"></VueApexCharts>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+    name: 'RadarChart',
+    components: {
+      VueApexCharts,
+
+    },
+    data () {
+        return {
+        series: [{
+            name: 'Test 1',
+            data: [80, 50, 30, 40, 100],
+          },
+          {
+            name: 'Test 2',
+            data: [40, 60, 40, 100, 10],
+          }],
+        
+        chartOptions: {
+          chart: {
+            // background: 'black',
+            width: '500%',
+            height: 500,
+            toolbar: {
+              show: false,
+            },
+            // width: '100%',
+            // height: '500px',
+            // offsetX: 0,
+            // parentHeightOffset: 0,
+          },
+          // title: {
+          //   text: 'Basic Radar Chart'
+          //   align: 'center'
+          // },
+          xaxis: {
+            categories: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5'],
+            labels: {
+              show: true,
+            }
+          },
+        },
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>

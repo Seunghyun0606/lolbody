3f53c0561bfcdf30e63c12c3dda2568726151af6
나호철 <sabsari5@naver.com>
2020-07-20 15:08:27 +0900

    Update Index.vue

---
+++ front/src/views/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="index">
+      <IndexLogo />
+      <IndexSearchBar />
+      <IndexIntro />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import IndexLogo from '@/components/index/IndexLogo.vue';
+import IndexSearchBar from '@/components/index/IndexSearchBar.vue';
+import IndexIntro from '@/components/index/IndexIntro.vue';
+
+export default {
+  name: 'Index',
+
+  components: {
+    IndexLogo,
+    IndexSearchBar,
+    IndexIntro,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>

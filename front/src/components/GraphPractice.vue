c2f42d5c7e7ac96224cd21459e15b99b12959a63
나호철 <sabsari5@naver.com>
2020-07-21 13:34:59 +0900

    Apexcharts practice

---
+++ front/src/components/GraphPractice.vue
@@ -0,0 +1,42 @@
+<template>
+  <apexchart width="500" type="radar" :options="options" :series="series"></apexchart>
+</template>
+
+<script>
+import apexchart from 'vue-apexcharts'
+
+export default {
+  components: {
+    apexchart
+  },
+  data() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: ['공격력', '안정성', '영향력']
+        },
+        yaxis: {
+          tickAmount: 5,
+          min: 0,
+          max: 100,
+        }
+      },
+      series: [{
+        name: '초반',
+        data: [30, 40, 45]
+      },
+      {
+        name: '후반',
+        data: [50, 40, 60]
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

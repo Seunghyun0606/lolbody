97f856c2a0f87f84e6f0a9bba91cee818c8ff0b5
나호철 <sabsari5@naver.com>
2020-07-21 14:40:43 +0900

    Update GraphPractice.vue
    Change graph by set time

--- front/src/components/GraphPractice.vue
+++ front/src/components/GraphPractice.vue
@@ -11,6 +11,20 @@ export default {
   },
   data() {
     return {
+      test: [1, 2, 3],
+      indexGraphSeries: {
+        idx: 0,
+        pre: [
+          [10, 20, 30],
+          [20, 10, 30],
+          [40, 50, 70],
+        ],
+        post: [
+          [20, 10, 60],
+          [40, 80, 50],
+          [30, 20, 10],
+        ]
+      },
       options: {
         chart: {
           id: 'vuechart-example'
@@ -26,14 +40,34 @@ export default {
       },
       series: [{
         name: '초반',
-        data: [30, 40, 45]
+        data: [30, 60, 50]
       },
       {
         name: '후반',
         data: [50, 40, 60]
       }]
     }
-  }
+  },
+  methods: {
+    changeIndexGraphSeries() {
+      if (this.indexGraphSeries.idx >= 2) {
+        this.indexGraphSeries.idx = 0
+      } else {
+        this.indexGraphSeries.idx += 1
+      }
+      this.series = [{
+        data: this.indexGraphSeries.pre[this.indexGraphSeries.idx]
+        },
+        {
+          data: this.indexGraphSeries.post[this.indexGraphSeries.idx]
+      }]
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.changeIndexGraphSeries();
+    }, 3000)
+  },
 }
 </script>
 

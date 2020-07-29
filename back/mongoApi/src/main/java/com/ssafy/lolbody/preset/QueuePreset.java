d242b2e4ec99313809955008b17132ed79b507b2
박진용 <jypark5826@gmail.com>
2020-07-29 16:40:11 +0900

    Update preset

--- back/mongoApi/src/main/java/com/ssafy/lolbody/preset/QueuePreset.java
+++ back/mongoApi/src/main/java/com/ssafy/lolbody/preset/QueuePreset.java
@@ -1,16 +1,12 @@
 package com.ssafy.lolbody.preset;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
+import com.ssafy.lolbody.api.Api;
 
 @Component
 public class QueuePreset implements CommandLineRunner{
@@ -19,28 +15,7 @@ public class QueuePreset implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		String result = "";
-		try {
-			URL url = new URL("http://static.developer.riotgames.com/docs/lol/queues.json");
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setConnectTimeout(5000);
-			con.setReadTimeout(5000);
-			
-			con.setRequestMethod("GET");
-			
-			StringBuilder sb = new StringBuilder();
-			if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(),"utf-8"));
-				String line;
-				while((line = br.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-				br.close();
-				result = sb.toString();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		String result = Api.getHttpRequest("http://static.developer.riotgames.com/docs/lol/queues.json");
 		JSONArray arr = new JSONArray(result);
 		for (int i = 0; i < arr.length(); i++) {
 			String json = arr.get(i).toString();
--- back/mongoApi/src/main/java/com/ssafy/lolbody/preset/SpellPreset.java
+++ back/mongoApi/src/main/java/com/ssafy/lolbody/preset/SpellPreset.java
@@ -1,14 +1,11 @@
 package com.ssafy.lolbody.preset;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.ssafy.lolbody.api.Api;
 
 @Component
 public class SpellPreset implements CommandLineRunner {
@@ -17,28 +14,7 @@ public class SpellPreset implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String result = "";
-		try {
-			URL url = new URL("http://ddragon.leagueoflegends.com/cdn/10.15.1/data/en_US/summoner.json");
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setConnectTimeout(5000);
-			con.setReadTimeout(5000);
-
-			con.setRequestMethod("GET");
-
-			StringBuilder sb = new StringBuilder();
-			if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
-				String line;
-				while ((line = br.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-				br.close();
-				result = sb.toString();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		String result = Api.getHttpRequest("http://ddragon.leagueoflegends.com/cdn/10.15.1/data/en_US/summoner.json");
 		JSONObject data = new JSONObject(result).getJSONObject("data");
 		for (String s : data.keySet()) {
 			JSONObject info = data.getJSONObject(s);

e57da8b67feb0d851615bd37e2dcdcae68fc38b0
나호철 <sabsari5@naver.com>
2020-08-03 11:46:38 +0900

    analysis_django

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -1,8 +1,8 @@
 import csv, os, time
 import numpy as np
 import pandas as pd
-from IPython.display import display
-from sklearn.preprocessing import MinMaxScaler
+# from IPython.display import display
+# from sklearn.preprocessing import MinMaxScaler
 
 pd.set_option('display.max_row', 500)
 
@@ -53,81 +53,86 @@ def create_folder(path):
     print()
 
 
-now = time.localtime()
-now = "%02d%02d%02d_%02d:%02d:%02d:" % (int(str(now.tm_year)[2:]), now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-positions = ['top', 'mid', 'jungle', 'carry', 'support']
-ranks = ['diamond', 'platinum', 'gold', 'silver', 'bronze', 'iron']
-for rank in ranks:
-    try:
-        file = open('./csv/%s/%s/%s_stastics.csv' % (now[:6], rank, now[:6]), 'w', newline='', encoding='utf-8')
-        csvfile = csv.writer(file)
-        csvfile.writerow([
-            'position', 
-            'visionScoreMean', 
-            'visionScoreStd', 
-            'csPerMinMean',
-            'csPerMinStd', 
-            'deathsRatioMean',
-            'deathsRatioStd',
-            'killAssistPerMinMean',
-            'killAssistPerMinStd',
-            'damageDealtPerMinMean',
-            'damageDealtPerMinStd',
-            'damageTakenPerMinMean',
-            'damageTakenPerMinStd',
-            'killsRatioMean',
-            'killsRatioStd',
-            ])
-    except:
-        continue
+def auto_mode():
+    now = time.localtime()
+    now = "%02d%02d%02d_%02d:%02d:%02d:" % (int(str(now.tm_year)[2:]), now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    positions = ['top', 'mid', 'jungle', 'carry', 'support']
+    ranks = ['diamond', 'platinum', 'gold', 'silver', 'bronze', 'iron']
+    for rank in ranks:
+        try:
+            file = open('./csv/%s/%s/%s_stastics.csv' % (now[:4], rank, now[:4]), 'w', newline='', encoding='utf-8')
+            csvfile = csv.writer(file)
+            csvfile.writerow([
+                'position', 
+                'visionScoreMean', 
+                'visionScoreStd', 
+                'csPerMinMean',
+                'csPerMinStd', 
+                'deathsRatioMean',
+                'deathsRatioStd',
+                'killAssistPerMinMean',
+                'killAssistPerMinStd',
+                'damageDealtPerMinMean',
+                'damageDealtPerMinStd',
+                'damageTakenPerMinMean',
+                'damageTakenPerMinStd',
+                'killsRatioMean',
+                'killsRatioStd',
+                ])
+        except:
+            continue
 
-    for position in positions:
-        data = pd.read_csv('./csv/200731/%s/200731_%s.csv' % (rank, position), encoding='utf-8')
-        # print(data)
+        for position in positions:
+            data = pd.read_csv('./csv/2008/%s/2008_%s.csv' % (rank, position), encoding='utf-8')
+            # print(data)
 
-        # 중복된 소환사 제거
-        data.drop_duplicates(keep='first', inplace=True)
-        # print(data)
+            # 중복된 소환사 제거
+            data.drop_duplicates(keep='first', inplace=True)
+            # print(data)
 
-        using_data = pd.DataFrame()
-        # 안정성
-        using_data['visionScore'] = data['visionScore']
-        using_data['csPerMin'] = data['totalMinionsKilled'] / (data['playtime'] / 60)
-        ## 낮을수록 좋기때문에 음수로 바꿔줌
-        using_data['deathsRatio'] = -data['deathsRatio']
+            using_data = pd.DataFrame()
+            # 안정성
+            using_data['visionScore'] = data['visionScore']
+            using_data['csPerMin'] = data['totalMinionsKilled'] / (data['playtime'] / 60)
+            ## 낮을수록 좋기때문에 음수로 바꿔서 사용해야함
+            using_data['deathsRatio'] = data['deathsRatio']
 
-        # 공격성
-        using_data['killAssistPerMin'] = (data['kills'] + data['assists']) / (data['playtime'] / 60)
-        using_data['damageDealtPerMin'] = data['totalDamageDealtToChampions'] / (data['playtime'] / 60)
-        using_data['damageTakenPerMin'] = data['totalDamageTaken'] / (data['playtime'] / 60)
+            # 공격성
+            using_data['killAssistPerMin'] = (data['kills'] + data['assists']) / (data['playtime'] / 60)
+            using_data['damageDealtPerMin'] = data['totalDamageDealtToChampions'] / (data['playtime'] / 60)
+            using_data['damageTakenPerMin'] = data['totalDamageTaken'] / (data['playtime'] / 60)
 
-        # 영향력
-        using_data['killsRatio'] = data['killsRatio']
+            # 영향력
+            using_data['killsRatio'] = data['killsRatio']
 
-        # 이상치 제거
-        idxs = []
-        for column in list(using_data.columns):
-            idxs += list(outliers(using_data[column]).index)
-            # csPerMin_outlier_idx = list(outliers(using_data.csPerMin).index)
-            # deathsRatio_outlier_idx = list(outliers(using_data.deathsRatio).index)
-        using_data = using_data.drop(idxs).reset_index(drop=True)
+            # 이상치 제거
+            idxs = []
+            for column in list(using_data.columns):
+                idxs += list(outliers(using_data[column]).index)
+                # csPerMin_outlier_idx = list(outliers(using_data.csPerMin).index)
+                # deathsRatio_outlier_idx = list(outliers(using_data.deathsRatio).index)
+            using_data = using_data.drop(idxs).reset_index(drop=True)
 
-        # 랭크별로 각 요소의 평균 삽입
-        row = [position, ]
-        for column in list(using_data.columns):
-            row.extend([using_data[column].mean(), using_data[column].std()])
-        print(row)
-        csvfile.writerow(row)
-        file.close()
-        # data_stastics = [d.mean(), d.std()]
-        # print(data_stastics)
+            # 랭크별로 각 요소의 평균 삽입
+            row = [position, ]
+            for column in list(using_data.columns):
+                row.extend([using_data[column].mean(), using_data[column].std()])
+            print(row)
+            csvfile.writerow(row)
+            
 
-        # z값으로 변환
-        # z_d = (d - d.mean()) / d.std()
-        # print(d)
-        # print(z_d)
 
-        # 정규화
-        # d = d.reset_index(drop=True)
-        # normal_d = pd.DataFrame(MinMaxScaler().fit_transform(d), columns=d.columns)
-        # print(normal_d)
+if __name__ == '__main__':
+    auto_mode()
+    # data_stastics = [d.mean(), d.std()]
+    # print(data_stastics)
+
+    # z값으로 변환
+    # z_d = (d - d.mean()) / d.std()
+    # print(d)
+    # print(z_d)
+
+    # 정규화
+    # d = d.reset_index(drop=True)
+    # normal_d = pd.DataFrame(MinMaxScaler().fit_transform(d), columns=d.columns)
+    # print(normal_d)

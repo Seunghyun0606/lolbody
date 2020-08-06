47f746eca76f600fae0720053fddd70aa40d47f4
임승현 <limseung10@gmail.com>
2020-08-07 04:18:40 +0900

    ProfileLineChart | fix lifeCycle bug

--- front/src/components/profile/ProfileLineChart.vue
+++ front/src/components/profile/ProfileLineChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <VueApexCharts height="150" type="line" :options="chartOptions" :series="series"></VueApexCharts>
+    <VueApexCharts height="150" type="line" :options="profileLineChartOption.chartOptions" :series="profileLineChartOption.series"></VueApexCharts>
   </div>
 </template>
 
@@ -16,96 +16,8 @@ export default {
     },
     computed: {
       ...mapState([
-        'matchDatas',
+        'profileLineChartOption',
       ])
-    },
-    created() {
-      this.change()
-    },
-    methods: {
-      // 일단 짜고 나중에 리팩토링하자.
-      // Line Chart 값넣기.
-      change() {
-        for ( var matchData of this.matchDatas ) {
-          if ( matchData.myTeam === 'blueTeam' ) {
-
-            this.series[0].data.unshift(matchData.blueTeam.teammate[matchData.myIndex].kda.toFixed(2))
-          }
-          else {
-            this.series[0].data.unshift(matchData.redTeam.teammate[matchData.myIndex].kda.toFixed(2))
-            
-          }
-          this.chartOptions.xaxis.categories.unshift(this.calcDate(matchData.timestamp))
-          
-          
-        }
-      },
-      calcDate(timestamp) {
-        let month = new Date(timestamp).getMonth() + 1 + '월 '
-        let day = new Date(timestamp).getDate() + '일 '
-        // let hour = new Date(timestamp).getHours() + '시'
-        
-        return month + day // + hour
-      },
-    },
-    data() {
-        return {
-
-          series: [
-            {
-              name: "KDA",
-              data: [] 
-            },
-            // {
-            //   name: "Lane2",
-            //   data: [3, 4, 5, 6, 5, 6.3, 7.5] 
-            // }
-          ],
-          chartOptions: {
-            chart: {
-              height: 150,
-              type: 'line',
-              zoom: {
-                enabled: false,
-              },
-              toolbar: {
-                show: false
-              },
-            },
-            colors: ['#77B6EA', '#545454'],
-            grid: {
-              borderColor: '#e7e7e7'
-            },
-            markers: {
-              size: 5,
-              shape: "circle",
-              radius: 1,
-            },
-            labels: [3, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            xaxis: {
-              labels: {
-                showDuplicates: false,
-                style: {
-                  fontSize: '10px',
-                }
-              },
-              tickAmount: 3,
-              categories: [],
-              type: 'category',
-              title: {
-                // text: '최근 10게임 KDA'
-              }
-            },
-            yaxis: {
-              title: {
-                text: 'KDA'
-              },
-              // tickAmount: 5,
-              // min: 0,
-              // max: 10
-            }
-          },
-        }
     },
 
 }
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -15,6 +15,63 @@ export default new Vuex.Store({
     userDatas: [],
     isIndex: '',
     radarChartDatas: [],
+    profileLineChartOption: {
+
+      series: [
+        {
+          name: "KDA",
+          data: [] 
+        },
+        // {
+        //   name: "Lane2",
+        //   data: [3, 4, 5, 6, 5, 6.3, 7.5] 
+        // }
+      ],
+      chartOptions: {
+        chart: {
+          height: 150,
+          type: 'line',
+          zoom: {
+            enabled: false,
+          },
+          toolbar: {
+            show: false
+          },
+        },
+        colors: ['#77B6EA', '#545454'],
+        grid: {
+          borderColor: '#e7e7e7'
+        },
+        markers: {
+          size: 5,
+          shape: "circle",
+          radius: 1,
+        },
+        labels: [3, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        xaxis: {
+          labels: {
+            showDuplicates: false,
+            style: {
+              fontSize: '10px',
+            }
+          },
+          tickAmount: 3,
+          categories: [],
+          type: 'category',
+          title: {
+            // text: '최근 10게임 KDA'
+          }
+        },
+        yaxis: {
+          title: {
+            text: 'KDA'
+          },
+          // tickAmount: 5,
+          // min: 0,
+          // max: 10
+        }
+      },
+    },
 
     // 호철
     searchSummonerIDs: [],
@@ -47,6 +104,29 @@ export default new Vuex.Store({
       // userData는 array로 오기 때문에 sperad시킴
       // 20.07.30 userData가 자유랭크 데이터도 넘기기때문에 스프레드 시키면안됨. 단일 오브젝트만 넣습니다.
       state.userDatas = [ ...state.userDatas, userDatas ]
+    },
+    setProfileLineChartOption(state, matchDatas) {
+      var calcDate = function(timestamp) {
+        let month = new Date(timestamp).getMonth() + 1 + '월 '
+        let day = new Date(timestamp).getDate() + '일 '
+        // let hour = new Date(timestamp).getHours() + '시'
+        
+        return month + day // + hour
+      }
+      state.profileLineChartOption.series[0].data = []
+      state.profileLineChartOption.chartOptions.xaxis.categories = []
+
+      for ( let matchData of matchDatas ) {
+        if ( matchData.myTeam === 'blueTeam' ) {
+
+          state.profileLineChartOption.series[0].data.unshift(matchData.blueTeam.teammate[matchData.myIndex].kda.toFixed(2))
+        }
+        else {
+          state.profileLineChartOption.series[0].data.unshift(matchData.redTeam.teammate[matchData.myIndex].kda.toFixed(2))
+        }
+        state.profileLineChartOption.chartOptions.xaxis.categories.unshift(calcDate(matchData.timestamp))
+          
+      }
     },
 
     // NavSearch toggle 용도
@@ -142,7 +222,8 @@ export default new Vuex.Store({
         return axios.get(SERVER_URL + `/profile/${userName}/${num}`)
         // return axios.get(`http://localhost:8888/profile/${userName}/${num}`)
             .then(res => {
-                commit('setMatchDatas', res.data)
+              commit('setMatchDatas', res.data)
+              commit('setProfileLineChartOption', res.data)
             }).catch(function (error) {
                 if (error.response) {
                     console.log(error.response.data);

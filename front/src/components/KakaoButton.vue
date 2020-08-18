7af230679efa1b5198388f3b4f2edbfce6fac6c9
나호철 <sabsari5@naver.com>
2020-08-18 15:59:33 +0900

    Front|Create KakaoButton Component

---
+++ front/src/components/KakaoButton.vue
@@ -0,0 +1,66 @@
+<template>
+  <a id="kakao-link-btn" @click="onClickKakaoButton">
+  <img
+    src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+  />
+</a>
+</template>
+
+<script>
+import Kakao from '@/plugins/kakao.js'
+Kakao.init('7b7a42d9cf2509fe7d9c7db809ef3807')
+
+export default {
+  name: 'KakaoButton',
+  data() {
+    return {
+      username: '',
+      url: 'https://lolbody.gq/Lolbody/',
+      badges: ['#학살자', '#농부', '#오지라퍼', '#대도둑']
+    }
+  },
+  methods: {
+    onClickKakaoButton() {
+      Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        // 여기 두 줄이 카톡에서 보임
+        title: this.username + '님 LoLBody 분석결과',
+        description: this.badges.toString().replace(/,/gi, ' '),
+        imageUrl:
+          'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        link: {
+          mobileWebUrl: this.url + this.username,
+          webUrl: this.url + this.username,
+        },
+      },
+      social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845,
+      },
+      // buttons: [
+      //   {
+      //     title: '웹으로 보기',
+      //     link: {
+      //       mobileWebUrl: this.url + this.username,
+      //       webUrl: this.url + this.username,
+      //     },
+      //   },
+      //   {
+      //     title: '앱으로 보기',
+      //     link: {
+      //       mobileWebUrl: this.url + this.username,
+      //       webUrl: this.url + this.username,
+      //     },
+      //   },
+      // ],
+    })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

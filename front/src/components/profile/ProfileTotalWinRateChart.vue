8e3b30a471e68567cdbc79b49f0528c2bde8dad2
KHR <gudfo19950@naver.com>
2020-08-19 13:55:30 +0900

    fix crash graph

--- front/src/components/profile/ProfileTotalWinRateChart.vue
+++ front/src/components/profile/ProfileTotalWinRateChart.vue
@@ -14,8 +14,11 @@ export default {
     DonutChart,
   },
   computed: {
+      ProfileTotalWinRateChart(){
+          return this.$store.state.ProfileTotalWinRateChart;
+      },
       series() {
-          return [this.$store.state.ProfileTotalWinRateChart.win, this.$store.state.ProfileTotalWinRateChart.lose]
+          return [this.ProfileTotalWinRateChart.win, this.ProfileTotalWinRateChart.lose]
       },
       chartOptions(){
         return {
@@ -24,72 +27,61 @@ export default {
                     offsetY: 10,
                     type: 'donut',
                 },
-        dataLabels: {
-          enabled: false
-        },
-        fill: {
-          colors: ['#1A73E8', '#B32824']  // 차트 색깔
-        },
-        legend: {
-          show: false,
-        },
-        labels: ["승리", "패배"],
-        plotOptions: {
-          pie: {
-            donut: {
-              labels: {
-                show: true,
-                name: {
-                  show: true,
-                  fontSize: '20px',
-                  fontFamily: 'Helvetica, Arial, sans-serif',
-                  fontWeight: 600,
-                  color: undefined,
-                  offsetY: 0,
-                  formatter: function (val) {
-                    return val
-                  }  // 호버시 승리, 패배 이름
-                },
-                value: {
-                  show: true,
-                  fontSize: '16px',
-                  fontFamily: 'Helvetica, Arial, sans-serif',
-                  fontWeight: 400,
-                  color: undefined,
-                  offsetY: 4,
-                  formatter: function (val) {
-                    return val
-                  }  // 호버 했을때, 승, 패 값
-                },
-                total: {
-                  show: true,
-                  label: "총 게임 수",
-                  fontSize: "14px",
-                  fontWeight: 600,
-                  fontFamily: 'Helvetica, Arial, sans-serif',
-
-                  color: 'black',
-                  formatter: function (w) {
-                        return w.globals.seriesTotals.reduce((a, b) => {
-                        return a + b
-                        }, 0)
-                    }
-                }
-              },
-            }
-          }
-        },
-        responsive: [{
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: "100%"
+            dataLabels: {
+              enabled: false
             },
-            // legend: {
-            //   position: 'bottom'
-            // }
-          }
-        }]
+            fill: {
+              colors: ['#1A73E8', '#B32824']  // 차트 색깔
+            },
+            legend: {
+              show: false,
+            },
+            labels: ["승리", "패배"],
+            plotOptions: {
+              pie: {
+                donut: {
+                  labels: {
+                    show: true,
+                    name: {
+                      show: true,
+                      fontSize: '20px',
+                      fontWeight: 600,
+                      offsetY: 0,
+                      formatter: function (val) {
+                        return val
+                      }  // 호버시 승리, 패배 이름
+                    },
+                    value: {
+                      show: true,
+                      fontSize: '16px',
+                      fontWeight: 400,
+                      offsetY: 4,
+                      formatter: function (val) {
+                        return val
+                      }  // 호버 했을때, 승, 패 값
+                    },
+                    total: {
+                      show: true,
+                      label: "총 게임 수",
+                      fontSize: "14px",
+                      fontWeight: 600,
+                      color: 'black',
+                    }
+                  },
+                }
+              }
+            },
+            responsive: [{
+              breakpoint: 480,
+              options: {
+                chart: {
+                  width: "100%"
+                },
+                // legend: {
+                //   position: 'bottom'
+                // }
+              }
+            }]
       } //return
     }//chartoption
   },
